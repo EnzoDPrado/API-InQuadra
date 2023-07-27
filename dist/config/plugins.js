cb3b055d3f9f38f47ae560ba363feab2269068ec
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-07-27 09:25:19 -0300

    Adding plugin config

---
+++ dist/config/plugins.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ env }) => ({
+    upload: {
+        config: {
+            providerOptions: {
+                localServer: {
+                    maxage: 300000
+                },
+                sizeLimit: 250 * 1024 * 1024 // 256mb in bytes
+            },
+            breakpoints: {
+                xlarge: 1920,
+                large: 1000,
+                medium: 750,
+                small: 500,
+                xsmall: 64
+            },
+        },
+    },
+});
