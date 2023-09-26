9d89d9bfa078b7416b06897402659f5fcd05ac6e
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-09-26 06:38:53 -0300

    Adding nodemailer config

--- config/plugins.ts
+++ config/plugins.ts
@@ -17,4 +17,21 @@ export default ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env.int("SMTP_PORT"),
+        auth: {
+          user: env("SMTP_USER"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: env("SMTP_FROM"),
+        defaultReplyTo: env("SMTP_FROM"),
+      },
+    },
+  },
 });
