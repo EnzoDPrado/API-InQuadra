8b951fbf214cb8f08bcc9fa91e81554ddac9caf5
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-09-26 06:44:30 -0300

    Updating fields in user-user-informations component

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -18,4 +18,21 @@ exports.default = ({ env }) => ({
             },
         },
     },
+    email: {
+        config: {
+            provider: "nodemailer",
+            providerOptions: {
+                host: env("SMTP_HOST"),
+                port: env.int("SMTP_PORT"),
+                auth: {
+                    user: env("SMTP_USER"),
+                    pass: env("SMTP_PASSWORD"),
+                },
+            },
+            settings: {
+                defaultFrom: env("SMTP_FROM"),
+                defaultReplyTo: env("SMTP_FROM"),
+            },
+        },
+    },
 });
